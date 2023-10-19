2fed701381ed50d6c3d7ad9046e0b91d97c28b3b
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-10-19 22:53:44 +0500

    Refactored(?)

--- src/main/java/edu/hw2/ConnectionException.java
+++ src/main/java/edu/hw2/ConnectionException.java
@@ -1,14 +1,16 @@
 package edu.hw2;
 
 public class ConnectionException extends RuntimeException {
-    public String messageOf;
-    public Exception reason;
+    public final String messageOf;
+    public final Exception reason;
 
     public ConnectionException() {
+        messageOf = "Ошибка";
+        reason = null;
     }
 
     public ConnectionException(String message, Exception cause) {
-        this.messageOf = message;
-        this.reason = cause;
+        messageOf = message;
+        reason = cause;
     }
 }
