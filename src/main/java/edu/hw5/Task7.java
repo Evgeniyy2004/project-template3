4a9ff4505965dbd40522fddf6f4ccef2d3ddedc3
evgen <evgeniy.ziganshin.04@mail.ru>
2023-11-10 00:36:08 +0500

    practically nothing

--- src/main/java/edu/hw5/Task7.java
+++ src/main/java/edu/hw5/Task7.java
@@ -7,6 +7,7 @@ public class Task7 {
     private Task7() {}
 
     public static final Pattern ONE = Pattern.compile("^(0|1)(0|1)0(0|1){0,}$");
+
     public  static final Pattern TWO = Pattern.compile("^(0|1)((0|1){1,}\\1|$)$");
     public static final Pattern THREE = Pattern.compile("^(0|1){1,3}$");
 }
