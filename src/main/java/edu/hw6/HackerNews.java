d5b3f8ad1f4473a3913dd07e5ab65e5a2187d973
evgen <evgeniy.ziganshin.04@mail.ru>
2023-11-11 12:58:50 +0500

    a

--- src/main/java/edu/hw6/HackerNews.java
+++ src/main/java/edu/hw6/HackerNews.java
@@ -1,10 +1,14 @@
 package edu.hw6;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import static java.net.http.HttpClient.newHttpClient;
 
 public class HackerNews {
     public long[] hackerNewsTopStories() {
@@ -13,11 +17,16 @@ public class HackerNews {
                 .uri(new URI("https://hacker-news.firebaseio.com/v0/topstories.json"))
                 .GET()
                 .header("nothing", "special")
-                .timeout(Duration.of())
+                .timeout(Duration.of(30, ChronoUnit.SECONDS))
+                .build();
+            var client = newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+            return client.body().ToObject<>;
 
-        } catch (URISyntaxException e) {
+        } catch (URISyntaxException | IOException | InterruptedException e) {
             return new long[0];
         }
+
     }
 
     public String news(long id) {
