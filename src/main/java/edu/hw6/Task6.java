0468d902444708ce289ab37f6ab34379faf83825
evgen <evgeniy.ziganshin.04@mail.ru>
2023-11-14 12:44:49 +0500

    norm

--- src/main/java/edu/hw6/Task6.java
+++ src/main/java/edu/hw6/Task6.java
@@ -1,31 +1,72 @@
 package edu.hw6;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.AbstractMap;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Task6 {
 
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    @SuppressWarnings("A lot of big numbers")
     public static boolean check(int port) {
         if (port > 65535 || port < 0) {
             throw new IllegalArgumentException();
         }
         try {
-            HashMap<Integer, AbstractMap.SimpleEntry<ServerSocket, DatagramSocket>> all =  new HashMap<>();
-            all.put(80, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(80)));
-            //если порт занят при создании socket будет ioexception
-            all.put(21, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(21)));
-            all.put(25, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(25)));
-            all.put(22, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(22)));
-            all.put(443, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(443)));
-            all.put(53, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(53)));
-            all.put(3306, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(3306)));
-            all.put(5432, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(5432)));
-            all.put(3389, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(3389)));
-            all.put(27017, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(27017)));
-            all.put(1521, new AbstractMap.SimpleEntry<>(new ServerSocket(80), new DatagramSocket(1521)));
+            HashMap<Integer, String> known =  new HashMap<>();
+            known.put(80, "HyperText Transfer Protocol");
+            known.put(21, "File Transfer Protocol");
+            known.put(25, "Simple Mail Transfer Protocol");
+            known.put(22, "Secure Shell");
+            known.put(443, "HyperText Transfer Protocol Secure");
+            known.put(53, "Domain Name System");
+            known.put(3306, "MySQL Database");
+            known.put(5432, "PostgreSQL Database");
+            known.put(3389, "Remote Desktop Protocol (RDP)");
+            known.put(27017, "MongoDB Database");
+            known.put(1521, "Oracle Database");
+            known.put(49153, "Remote Procedure Call");
+            known.put(23, "Telnet");
+            known.put(110, "POP3");
+            known.put(143, "IMAP");
+            known.put(445, "SMB");
+            HashMap<Integer, Map.Entry<ServerSocket, DatagramSocket>> unknown =  new HashMap<>();
+            for (int i = 0; i <= 49151; i++) {
+                if (known.containsKey(i)) {
+                    continue;
+                }
+                try (Socket ignored = new Socket("localhost", i)) {
+                    known.put(i,"");
+                } catch (ConnectException e) {
+                    unknown.put(i, new AbstractMap.SimpleEntry<>(new ServerSocket(i), new DatagramSocket(i)));
+                }
+            }
+            int curr = 1;
+            LOGGER.info("Протокол   Порт  Сервис");
+            for (int i = 0; i < 20; i++) {
+                    if (curr > 49151) {
+                        break;
+                    }
+                    String c = known.getOrDefault(curr, "");
+                    String p = (curr % 2 == 0) ? "UDP" : "TCP";
+
+            }
+
+
+            try (Socket ignored = new Socket("localhost", port)) {
+                return true;
+            } catch (ConnectException e) {
+                return false;
+            }
+
         } catch (IOException e) {
         }
     }
