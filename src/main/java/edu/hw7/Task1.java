a9afbfa5d3e6e26c79270e19c9157177abbffb5c
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-11-23 00:33:02 +0500

    nthng

---
+++ src/main/java/edu/hw7/Task1.java
@@ -0,0 +1,20 @@
+package edu.hw7;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Task1 {
+
+    private Task1() {
+    }
+
+    public static Long someThreads(Long numberOfThreads) {
+        var value = new AtomicLong(0L);
+        for(long j =0; j < numberOfThreads; j++) {
+            var thread = new Thread(()->value.addAndGet(1));
+            thread.start();
+        }
+        while (value.get() != numberOfThreads) {
+        }
+        return value.get();
+    }
+}
---
+++ src/main/java/edu/hw7/Task2.java
@@ -0,0 +1,16 @@
+package edu.hw7;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Task2 {
+
+    private Task2() {
+    }
+
+    public static Long factorial(Long number) {
+        List<Long> before = new Vector<>();
+        for (long j = 1; j <= number; j++) before.add(j);
+        return before.parallelStream().reduce(1L, Math::multiplyExact);
+    }
+}
