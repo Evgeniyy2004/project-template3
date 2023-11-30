33c53ba23781b42d121ba231b04b7387e156ef2d
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-11-30 18:59:31 +0500

    i did nothing

--- src/main/java/edu/hw8/OneThreadPasswords.java
+++ src/main/java/edu/hw8/OneThreadPasswords.java
@@ -28,7 +28,7 @@ public class OneThreadPasswords {
         MessageDigest md = MessageDigest.getInstance("MD5");
         var theMD5digest = new String(md.digest(bytesOfMessage));
         if (all.get(theMD5digest) != null) {
-            var name = all.get(curr);
+            var name = all.get(theMD5digest);
             put.put(curr, name);
         }
     }
