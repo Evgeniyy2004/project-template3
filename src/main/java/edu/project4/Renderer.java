266f871addd57b048fb4ae515fe6a7f5f414a615
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-12-08 17:31:28 +0500

    resolving conflicts

--- src/main/java/edu/project4/Renderer.java
+++ src/main/java/edu/project4/Renderer.java
@@ -6,16 +6,13 @@ import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 //import static lombok.launch.PatchFixesHider.Transform.transform;
 
 //@FunctionalInterface
 public class Renderer {
-
-    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    public static ConcurrentHashMap<Point, int[]> goals = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<Point, int[]> goals = new ConcurrentHashMap<>();
     private static final double ANTIACCURACY = 1e-6;
 
     FractalImage render(
@@ -26,20 +23,19 @@ public class Renderer {
         int samples,
         int iterPerSample
     ) {
-
+        var random2 = ThreadLocalRandom.current().nextInt(2 + 2 + 1);
         for (int num = 0; num < samples; num++) {
             var currx = ThreadLocalRandom.current().nextDouble(world.x(), (world.x() + world.width()) + ANTIACCURACY);
             var curry = ThreadLocalRandom.current().nextDouble(world.y(), (world.y() + world.height()) + ANTIACCURACY);
-            var random3 = ThreadLocalRandom.current().nextInt(colors.size());
-            for (int step = 0; step < iterPerSample; step++) {
+            var random3 = 1;
 
-                var random2 = ThreadLocalRandom.current().nextInt(2 + 2 + 1);
+            for (int yy = 0; yy < iterPerSample; yy++) {
                 var random = ThreadLocalRandom.current().nextInt(variations.size());
+
                 var variation = variations.get(random);
                 var x = variation.get(0) * currx + variation.get(1) * curry + variation.get(2);
                 var y = variation.get(2 + 1) * currx + variation.get(2 * 2) * curry + variation.get(2 * 2 + 1);
                 var pw = new Point2D.Double();
-
 
                 if (random2 == 0) {
                     pw.x = (x) / (x * x + y * y);
@@ -61,46 +57,42 @@ public class Renderer {
                 if (!world.contains(pw)) {
                     continue;
                 }
-                if (step >= 0) {
-                    if (goals.get(new Point(
-                        ((int) (pw.x * canvas.width() / world.width())),
-                        ((int) (pw.y * canvas.height() / world.height()))
-                    )) == null) {
-                        goals.put(
-                            new Point(
-                                Math.abs((int) (pw.x * canvas.width() / world.width())),
-                                (int) (pw.y * canvas.height() / world.height())
-                            ),
-                            new int[] {colors.get(random3).get(0), colors.get(random3).get(1),
-                                colors.get(random3).get(2), 1}
-                        );
-                    } else {
-                        //Попали не в первый раз, считаем так:
-                        var pixel = goals.get(new Point(
+
+                if (goals.get(new Point(
+                    ((int) (pw.x * canvas.width() / world.width())),
+                    ((int) (pw.y * canvas.height() / world.height()))
+                )) == null) {
+                    goals.put(
+                        new Point(
                             Math.abs((int) (pw.x * canvas.width() / world.width())),
                             (int) (pw.y * canvas.height() / world.height())
-                        ));
-                        goals.put(
-                            new Point(
-                                Math.abs((int) (pw.x * canvas.width() / world.width())),
-                                Math.abs((int) (pw.y * canvas.height() / world.height()))
-                            ),
-                            new int[] {(pixel[0] + colors.get(random3).get(0)) / 2,
-                                (pixel[1] + colors.get(random3).get(1)) / 2,
-                                (pixel[2] + colors.get(random3).get(2)) / 2,
-                                pixel[2 + 1] + 1}
-                        );
-                    }
+                        ),
+                        new int[] {colors.get(random3).get(0), colors.get(random3).get(1),
+                            colors.get(random3).get(2), 1}
+                    );
+                } else {
+                    //Попали не в первый раз, считаем так:
+                    var pixel = goals.get(new Point(
+                        Math.abs((int) (pw.x * canvas.width() / world.width())),
+                        (int) (pw.y * canvas.height() / world.height())
+                    ));
+                    goals.put(
+                        new Point(
+                            Math.abs((int) (pw.x * canvas.width() / world.width())),
+                            Math.abs((int) (pw.y * canvas.height() / world.height()))
+                        ),
+                        new int[] {(pixel[0] + colors.get(random3).get(0)) / 2,
+                            (pixel[1] + colors.get(random3).get(1)) / 2,
+                            (pixel[2] + colors.get(random3).get(2)) / 2,
+                            pixel[2 + 1] + 1}
+                    );
                 }
-
             }
-
         }
         return canvas;
-
     }
 
-    public static void notEqual() {
+    public void notEqual() {
         var max = 0.0;
         var gamma = (1 + 1.0 / (2.0 * 2.0 * 2.0 + 2.0)) * 2;
         for (Point p : goals.keySet()) {
