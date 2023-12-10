546119c136803dbc2339b0a672b05b8b454fc7ec
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-12-11 01:07:42 +0500

    how f i can make generic annotation

--- src/main/java/edu/hw10/Task1.java
+++ src/main/java/edu/hw10/Task1.java
@@ -1,37 +1,42 @@
 package edu.hw10;
 
+import java.lang.reflect.Parameter;
 import java.util.Random;
 import java.util.UUID;
 
 public class Task1 {
     Object nextObject(Class c)
         throws Exception {
-        var instance = c.newInstance();
-        var types = new Class [c.getConstructor().getParameterTypes().length];
+        var types = c.getConstructor().getParameterTypes();
+        var parameters = c.getConstructor().getParameters();
         var arguments = new Object[types.length];
         for (int i =0; i < types.length; i++) {
-            /*field.setAccessible(true);
-            Object value = getRandomValueForField(field);
-            field.set(instance, value);*/
-            arguments[i]= getRandomValueForField(types[i]);
+            arguments[i]= getRandomValueForField(types[i], parameters[i]);
         }
         return c.getConstructor(types).newInstance(arguments);
     }
-    private Object getRandomValueForField(Class parameter) throws Exception {
+    private Object getRandomValueForField(Class parameter, Parameter p) throws Exception {
         Class type = parameter;
-        // Note that we must handle the different types here! This is just an
-        // example, so this list is not complete! Adapt this to your needs!
+
         if(type.isEnum()) {
             Object[] enumValues = type.getEnumConstants();
             return enumValues[new Random().nextInt(enumValues.length)];
         } else if(type.equals(Integer.TYPE) || type.equals(Integer.class)) {
-            return new Random().nextInt();
+            var left = (p.isAnnotationPresent(Min.class))? p.getAnnotation(Min.class).value(): Integer.MIN_VALUE;
+            var right = (p.isAnnotationPresent(Max.class))? p.getAnnotation(Max.class).value(): Integer.MAX_VALUE;
+            return new Random().nextInt((int)left,(int)right);
         } else if(type.equals(Long.TYPE) || type.equals(Long.class)) {
-            return new Random().nextLong();
+            var left = (p.isAnnotationPresent(Min.class))? p.getAnnotation(Min.class).value(): Long.MIN_VALUE;
+            var right = (p.isAnnotationPresent(Max.class))? p.getAnnotation(Max.class).value(): Long.MAX_VALUE;
+            return new Random().nextLong((long) left,(long) right);
         } else if(type.equals(Double.TYPE) || type.equals(Double.class)) {
-            return new Random().nextDouble();
+            var left = (p.isAnnotationPresent(Min.class))? p.getAnnotation(Min.class).value(): Double.MIN_VALUE;
+            var right = (p.isAnnotationPresent(Max.class))? p.getAnnotation(Max.class).value(): Double.MAX_VALUE;
+            return new Random().nextDouble((double) left, (double) right);
         } else if(type.equals(Float.TYPE) || type.equals(Float.class)) {
-            return new Random().nextFloat();
+            var left = (p.isAnnotationPresent(Min.class))? p.getAnnotation(Min.class).value(): Float.MIN_VALUE;
+            var right = (p.isAnnotationPresent(Max.class))? p.getAnnotation(Max.class).value(): Float.MAX_VALUE;
+            return new Random().nextFloat((float) left, (float) right);
         } else if(type.equals(String.class)) {
             return UUID.randomUUID().toString();
         } else {
@@ -43,12 +48,10 @@ public class Task1 {
         throws Exception {
         var instance = c.newInstance();
         var types = new Class [c.getMethod(method).getParameterTypes().length];
+        var parameters = c.getConstructor().getParameters();
         var arguments = new Object[types.length];
         for (int i =0; i < types.length; i++) {
-            /*field.setAccessible(true);
-            Object value = getRandomValueForField(field);
-            field.set(instance, value);*/
-            arguments[i]= getRandomValueForField(types[i]);
+            arguments[i]= getRandomValueForField(types[i], parameters[i]);
         }
         return c.getMethod(method).invoke(instance,arguments);
     }
