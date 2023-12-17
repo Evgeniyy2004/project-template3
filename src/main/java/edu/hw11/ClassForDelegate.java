89bef891df36f6d4697d3860c43f6e34f9f53fdb
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-12-17 23:27:59 +0500

    aaaaaaaaaa

--- src/main/java/edu/hw11/ClassForDelegate.java
+++ src/main/java/edu/hw11/ClassForDelegate.java
@@ -2,6 +2,7 @@ package edu.hw11;
 
 import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("HideUtilityClassConstructor")
 public class ClassForDelegate {
     public static int length(@NotNull String count) {
         return count.length();
--- src/main/java/edu/hw11/FibClass.java
+++ src/main/java/edu/hw11/FibClass.java
@@ -1,9 +1,16 @@
 package edu.hw11;
 
+
+@SuppressWarnings("HideUtilityClassConstructor")
 public class FibClass {
     public static long fib(int n) {
-        if (n == 0 ) return 0;
-        if (n==1) return 1;
-        else return fib(n-1)+fib(n-2);
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
     }
 }
